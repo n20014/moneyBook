de32eb2d749b769b49699f13c3b2711296041ec3
vagrant <vagrant@ubuntu2004.localdomain>
2021-01-27 11:26:41 +0000

    first

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import './App.css'
+
+class JyankenGamePage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { human: null, component: null }
+  }
+
+  pon (human_hand) {
+    const computer_hand = Math.floor(Math.random() * 3)
+    this.setState({ human: human_hand, computer: computer_hand })
+  }
+
+  judge () {
+    if (this.state.human == null) {
+      return null
+    } else {
+      return (this.state.computer - this.state.human + 3) % 3
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>じゃんけんポン! </h1>
+        <JyankenBox actionPon={te => this.pon(te)} />
+        <ScoreBox
+          human={this.state.human}
+          computer={this.state.computer}
+          judgment={this.judge()}
+        />
+      </div>
+    )
+  }
+}
+
+const JyankenBox = props => {
+  return (
+    <div>
+      <button onClick={() => props.actionPon(0)}>グー</button>
+      <button onClick={() => props.actionPon(1)}>チョキ</button>
+      <button onClick={() => props.actionPon(2)}>パー</button>
+    </div>
+  )
+}
+
+JyankenBox.propTypes = {
+  actionPon: PropTypes.func
+}
+
+const ScoreBox = (props) => {
+  const teString = ["グー","チョキ","パー"]
+  const judgmentString = ["引き分け","勝ち","負け"]
+  return( 
+    <table>
+      <tbody>
+        <tr><th>あなた</th></td>{teString[props.human]}</td></tr>
+        <tr><th>Computer</th><td>{teString[props.computer]}</td></tr>
+        <tr><th>勝敗</th><td>{teString[props.judgment]}</td></tr>
+      </tbody>
+    </table>
+  )
+}
+
+ScoreBox.propTypes = {
+  human: PropTypes.number,
+  computer: PropTypes.number,
+  judgment: PropTypes.number
+}
+
+export default JyankenGamePage
+
